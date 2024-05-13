6763685de533baed9db86f4c73c0e2716ae0d18e
Faith Peddie <faithpeddie@outlook.com>
2024-05-13 14:57:40 +0100

    Faith completed Artist Class

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,2 +1,20 @@
 public class Artist {
+
+    // properties
+    private String name;
+
+    // constructor
+    public Artist(String name){
+        this.name = name;
+    }
+
+    // methods
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
 }
--- src/test/java/ArtistTest.java
+++ src/test/java/ArtistTest.java
@@ -1,2 +1,33 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ArtistTest {
+
+    Artist artist;
+
+    @BeforeEach
+    // given
+    public void setUp(){
+        artist = new Artist("Van Gogh");
+    }
+
+    @Test
+    public void canGetName(){
+        // when
+        String result = artist.getName();
+        // then
+        assertThat(result).isEqualTo("Van Gogh");
+    }
+
+    @Test
+    public void canSetName(){
+        // when
+        artist.setName("Picasso");
+        String result = artist.getName();
+        // then
+        assertThat(result).isEqualTo("Picasso");
+    }
+
 }
