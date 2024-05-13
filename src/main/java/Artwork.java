774e668c38ddd45fe8940c30aeaaddfd25eeb536
Rabin Kandel <kandelrabin@live.com>
2024-05-13 15:25:45 +0100

    Rabin completed Artwork class

--- src/main/java/Artwork.java
+++ src/main/java/Artwork.java
@@ -1,2 +1,33 @@
 public class Artwork {
+
+    // properties
+    private String title;
+    private Artist artist;  // String??
+    private double price;
+
+    public Artwork(String title, double price, String artistName){
+        this.title = title;
+        this.price = price;
+        this.artist = new Artist(artistName);
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String newTitle){
+        this.title = newTitle;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double newPrice){
+        this.price = newPrice;
+    }
+
+    public String getArtistName(){
+        return this.artist.getName();
+    }
 }
--- src/test/java/ArtworkTest.java
+++ src/test/java/ArtworkTest.java
@@ -1,2 +1,50 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ArtworkTest {
+
+    // given
+    Artwork artwork;
+
+    @BeforeEach
+    public void setup(){
+        artwork = new Artwork("The Starry Night", 100000000.00, "Van Gogh");
+    }
+
+    @Test
+    public void canGetTitle(){
+        // when
+        String result = artwork.getTitle();
+        // then
+        assertThat(result).isEqualTo("The Starry Night");
+    }
+
+    @Test
+    public void canSetTitle(){
+        artwork.setTitle("The Potato Eaters");
+        String result = artwork.getTitle();
+        assertThat(result).isEqualTo("The Potato Eaters");
+    }
+
+    @Test
+    public void canGetPrice(){
+        double result = artwork.getPrice();
+        assertThat(result).isEqualTo(100000000.00);
+    }
+
+    @Test
+    public void canSetPrice(){
+        artwork.setPrice(5000000.00);
+        double result = artwork.getPrice();
+        assertThat(result).isEqualTo(5000000.00);
+    }
+
+    @Test
+    public void canGetArtistName(){
+        String result = artwork.getArtistName();
+        assertThat(result).isEqualTo("Van Gogh");
+    }
+
 }
