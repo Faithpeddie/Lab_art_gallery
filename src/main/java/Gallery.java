bd625bebcc021fababf49e39b426ed495eb87aa8
Faith Peddie <faithpeddie@outlook.com>
2024-05-13 16:29:42 +0100

    Faith has done Gallery Class

--- src/main/java/Gallery.java
+++ src/main/java/Gallery.java
@@ -1,2 +1,47 @@
+import java.util.ArrayList;
+
 public class Gallery {
+
+    private String name;
+    private double till;
+    private ArrayList<Artwork> stock;
+
+    public Gallery(String name){
+        this.name = name;
+        this.till = 0.00;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public double getTillBalance(){
+        return this.till;
+    }
+
+    public void depositToTill(double amount){
+        this.till += amount;
+    }
+
+    public Artwork getArtworkFromStock(int index){
+       return this.stock.get(index);
+    }
+
+    public int getSizeOfStock(){
+        return this.stock.size();
+    }
+
+    public void addArtworkToStock(Artwork artwork){
+        this.stock.add(artwork);
+    }
+
+    public ArrayList<Artwork> getStock(){
+        return this.stock;
+    }
+
 }
--- src/test/java/GalleryTest.java
+++ src/test/java/GalleryTest.java
@@ -1,2 +1,73 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class GalleryTest {
+
+    Gallery gallery;
+
+    @BeforeEach
+    public void setUp(){
+        gallery = new Gallery("Saatchi Gallery");
+    }
+
+    @Test
+    public void canGetName(){
+        String result = gallery.getName();
+        assertThat(result).isEqualTo("Saatchi Gallery");
+    }
+
+    @Test
+    public void canSetName(){
+        gallery.setName("British Museum");
+        String result = gallery.getName();
+        assertThat(result).isEqualTo("British Museum");
+    }
+
+    @Test
+    public void canGetTillBalance(){
+        double result = gallery.getTillBalance();
+        assertThat(result).isEqualTo(0.00);
+    }
+
+    @Test
+    public void canDepositToTill(){
+        gallery.depositToTill(1000.00);
+        double result = gallery.getTillBalance();
+        assertThat(result).isEqualTo(1000.00);
+    }
+
+    @Test
+    public void canGetSizeOfStock(){
+        int result = gallery.getSizeOfStock();
+        assertThat(result).isEqualTo(0);
+    }
+
+
+    @Test
+    public void canGetArtworkFromStock(){
+        // given
+        Artwork artwork = new Artwork("The Starry Night", 1000000000, "Van Gogh");
+        gallery.addArtworkToStock(artwork);
+        Artwork result = gallery.getArtworkFromStock(0);
+        // when
+        int size = gallery.getSizeOfStock();
+        // then
+        if(size != 0){
+            assertThat(result).isEqualTo(artwork);
+        }
+        else{
+            assertThat(gallery.getStock().isEmpty()).isEqualTo(true);
+        }
+    }
+
+    @Test
+    public void canAddArtworkToStock(){
+        Artwork artwork = new Artwork("The Starry Night", 1000000000, "Van Gogh");
+        gallery.addArtworkToStock(artwork);
+        Artwork result = gallery.getArtworkFromStock(0);
+        assertThat(result).isEqualTo(artwork);
+    }
+
 }
